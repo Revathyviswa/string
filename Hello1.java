5686b5186adeef9213aa7cce242bde410bf26d08
Revathyviswa <hemarev05@gmail.com>
2016-07-16 14:36:03 +0530

    Add files via upload

---
+++ Hello1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello1;
+
+/**
+ *
+ * @author Student
+ */
+public class Hello1 {
+
+ 
+    public static void main(String[] args) {
+     int i=0;
+     String s="Hello";
+     for(i=0;i<5;i++)
+     {
+         System.out.println("Hello");
+     }
+    }
+}
